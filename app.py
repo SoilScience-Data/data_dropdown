2ebf2e4a9a7f1a098560b3e61c1364cfe815294f
SoilScience-Data <rika.a.soilscience@gmail.com>
2025-08-24 11:30:30 +0200

    app.py

--- app.py
+++ app.py
@@ -7,12 +7,13 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
+import openpyxl
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])  # Try FLATLY, LUX, or CYBORG
 server = app.server
 
 excel_url = "https://raw.githubusercontent.com/SoilScience-Data/data_dropdown/240e1867bd9565874cf94586852d73ffb99470cd/All_Data_LMM.xlsx"
-df1 = pd.read_excel(excel_url, parse_dates=True, na_values={"NAN"})
+df1 = pd.read_excel(excel_url,engine="openpyxl", parse_dates=True, na_values={"NAN"})
 
 df1['ID']=df1['Tillage'].astype(str)+"_"+df1['Fertilizer'].astype(str)+"_"+df1['Cover'].astype(str)
 
@@ -119,6 +120,7 @@ def update_plot(y_var, group1, group2, rep):
 if __name__ == "__main__":
 
     app.run(debug=True)
+
 
 
 
